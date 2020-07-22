3842c531282a45eeda4ce54d2258f7751763092e
zaitsev-adnrey <57433795+zaitsev-adnrey@users.noreply.github.com>
2020-07-22 15:21:51 +0500

    fix output bugs

--- command.php
+++ command.php
@@ -1,16 +1,20 @@
 <?php
 
 if ( ! class_exists( 'WP_CLI' ) ) {
-	return;
+    return;
 }
-/**
- * Изменение прав доступа wp-config на всех найденых сайтах
- * Пример wp cmod /var/www 0640
- *
- * @when before_wp_load
- */
-$chmod = function($args){
-    $install_options = array(
+function findwp($catalog){
+    $find_options = array(
+                      'return'     => true,   // Return 'STDOUT'; use 'all' for full object.
+                      'parse'      => 'json', // Parse captured STDOUT to JSON array.
+                      'launch'     => false,  // Reuse the current process.
+                      'exit_error' => true,   // Halt script execution on error.
+                    );
+    $paths = WP_CLI::runcommand('find '. $catalog .'  --field=wp_path --format=json', $find_options);
+    return $paths;
+}
+function issetwpfind(){
+        $install_options = array(
                       'return'     => true,   // Return 'STDOUT'; use 'all' for full object.
                       'parse'      => 'json', // Parse captured STDOUT to JSON array.
                       'launch'     => false,  // Reuse the current process.
@@ -34,46 +38,44 @@ $chmod = function($args){
                      );
      WP_CLI::runcommand('package install wp-cli/find-command',$options);
     }
-    $find_options = array(
-                      'return'     => true,   // Return 'STDOUT'; use 'all' for full object.
-                      'parse'      => 'json', // Parse captured STDOUT to JSON array.
-                      'launch'     => false,  // Reuse the current process.
-                      'exit_error' => true,   // Halt script execution on error.
-                    );
-    $paths = WP_CLI::runcommand('find '. $args[0] .'  --field=wp_path --format=json', $find_options);
-    foreach ($paths as $key => $path) {
+}
+/**
+ * Изменение прав доступа wp-config на всех найденых сайтах
+ * Пример wp cmod /var/www 0640
+ *
+ * @when before_wp_load
+ */
+$chmod = function($args){
+    issetwpfind();
+    $paths = findwp($args[0]);
+    foreach ($paths as $path) {
         //WP_CLI::line($path);
         $path_options = array(
                        'return'     => true,   // Return 'STDOUT'; use 'all' for full object.
                         );
         $config_path = WP_CLI::runcommand('config path --path=' . $path .' ',$path_options);
         //WP_CLI::line($config_path);
-        exec("chmod -c " . $args[1] . " ". $config_path ."", $info);
-        if(!empty($info[0])){
-           WP_CLI::line($info[0]);
-        }
+        system("chmod -c " . $args[1] . " ". $config_path ."");
     }
+
 };
 WP_CLI::add_command('chmod', $chmod);
+/**
+ * Просмотр текущих прав доступа wp-config на всех найденых сайтах
+ * Пример wp lsl /var/www 
+ *
+ * @when before_wp_load
+ */
 $config_lsl = function($args){
-	$find_options = array(
-                      'return'     => true,   // Return 'STDOUT'; use 'all' for full object.
-                      'parse'      => 'json', // Parse captured STDOUT to JSON array.
-                      'launch'     => false,  // Reuse the current process.
-                      'exit_error' => true,   // Halt script execution on error.
-                    );
-    $paths = WP_CLI::runcommand('find '. $args[0] .'  --field=wp_path --format=json', $find_options);
+    issetwpfind();
+    $paths = findwp($args[0]);
     foreach ($paths as $key => $path) {
         //WP_CLI::line($path);
         $path_options = array(
                        'return'     => true,   // Return 'STDOUT'; use 'all' for full object.
                         );
         $config_path = WP_CLI::runcommand('config path --path=' . $path .' ',$path_options);
-        //WP_CLI::line($config_path);
-        exec("ls -l ". $config_path ."", $info);
-        if(!empty($info[0])){
-           WP_CLI::line($info[0]);
-        }
+         system("ls -l ". $config_path ."");
     }
 };
 WP_CLI::add_command('lsl', $config_lsl);
